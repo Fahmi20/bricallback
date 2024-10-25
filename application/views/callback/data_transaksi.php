435a2ef41972b0aca66adec39a3b9e12e76b225f
Fahmi <fahmi4331@gmail.com>
2024-10-25 13:09:52 +0800

    modify Input Start Date

--- application/views/callback/data_transaksi.php
+++ application/views/callback/data_transaksi.php
@@ -9,92 +9,54 @@
     <div class="col-lg-12 grid-margin">
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title">Data Transaksi</h4>
-                <div class="row">
-                    <div class="col-6">
-                        <div class="input-group date datepicker" id="datePickerStart">
-                            <input type="text" class="form-control" id="start_date" placeholder="Start Date">
-                            <span class="input-group-text input-group-addon"><i data-feather="calendar"></i></span>
-                        </div>
+                <h4 class="card-title text-center mb-4">Data Transaksi</h4>
+                <div class="row g-3">
+                    <!-- Input Start Date -->
+                    <div class="col-md-6">
+                        <label for="startDate" class="form-label fw-bold">Start Date</label>
+                        <input type="text" class="form-control" id="startDate" placeholder="Select Start Date"
+                            autocomplete="off">
                     </div>
-                    <div class="col-6">
-                        <div class="input-group date datepicker" id="datePickerEnd">
-                            <input type="text" class="form-control" id="end_date" placeholder="End Date">
-                            <span class="input-group-text input-group-addon"><i data-feather="calendar"></i></span>
-                        </div>
-                    </div>
-                    <div class="col-12 mt-3">
-                        <button id="search" class="btn btn-primary" style="width: 100%;"> Search</button>
+                    <!-- Button Search -->
+                    <div class="col-md-12 mt-4">
+                        <button id="search" class="btn btn-primary btn-lg w-100">
+                            Search
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div id="barang_masuk">
+
+    <div id="transaksi_masuk">
         <div class="col-lg-12 grid-margin">
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Transaksi Masuk</h4>
                     <div class="table-responsive mt-3">
-                        <table id="table_po" style="width: 100%;" class="table">
+                        <table id="table_po" class="table" style="width: 100%;">
                             <thead>
                                 <tr>
                                     <th>No</th>
-                                    <th style="text-align: center;">Tanggal Masuk</th>
-                                    <th style="text-align: center;">Batch Number</th>
-                                    <th style="text-align: center;">Nama Barang</th>
-                                    <th style="text-align: center;">Qty</th>
-                                    <th style="text-align: center;">Satuan</th>
-                                    <th style="text-align: center;">Expired Date</th>
+                                    <th>ID PARTNER</th>
+                                    <th>NO CUSTOMER</th>
+                                    <th>NOMOR VIRTUAL ACCOUNT</th>
+                                    <th>TRX ID</th>
+                                    <th>TOTAL AMOUNT</th>
+                                    <th>TANGGAL TRANSAKSI</th>
+                                    <th>ADDITIONAL INFO</th>
+                                    <th>CHANNEL</th>
+                                    <th>DESKRIPSI</th>
+                                    <th>SUMBER AKUN VA</th>
+                                    <th>TELLER ID</th>
                                 </tr>
                             </thead>
-                            <tbody id="table_barang_masuk">
-                            </tbody>
+                            <tbody id="table_transaksi_masuk"></tbody>
                         </table>
                     </div>
                     <button id="print" class="mt-3 btn btn-secondary btn-sm"><i class="fa fa-print"></i> Print</button>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div id="doc_print">
-        <style>
-            .table-bordered thead th {
-                border: 2px solid;
-            }
-
-            .table-bordered tbody td {
-                border: 2px solid;
-            }
-
-            .table-bordered {
-                border-color: black;
-                width: 100%;
-                border-collapse: collapse;
-            }
-        </style>
-        <div class="col-lg-12">
-            <hr>
-            <h4 style="text-align: center;">Data Barang Masuk</h4>
-            <hr>
-            <div class="table-responsive mt-3">
-                <table id="table_po" style="width: 100%;" class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th style="text-align: center;">Tanggal Masuk</th>
-                            <th style="text-align: center;">Batch Number</th>
-                            <th style="text-align: center;">Nama Barang</th>
-                            <th style="text-align: center;">Qty</th>
-                            <th style="text-align: center;">Satuan</th>
-                            <th style="text-align: center;">Expired Date</th>
-                        </tr>
-                    </thead>
-                    <tbody id="print_table_barang_masuk">
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
