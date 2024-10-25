4184420b57d83963125c21d861b2ca79e0db4600
Fahmi <fahmi4331@gmail.com>
2024-10-25 13:08:00 +0800

    add file HIT PHP

---
+++ application/libraries/Hit.php
@@ -0,0 +1,44 @@
+<?php
+
+class Hit {
+
+    public function general_hit($url, $post = null) {
+        $usecookie = __DIR__ . "/cookie.txt";
+        $header[] = 'Content-Type: application/json';
+        $header[] = "Accept-Encoding: gzip, deflate";
+        $header[] = "Cache-Control: max-age=0";
+        $header[] = "Connection: keep-alive";
+        $header[] = "Accept-Language: en-US,en;q=0.8,id;q=0.6";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_ENCODING, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36");
+
+        if ($post) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+        }
+
+        $result = curl_exec($ch);
+
+        if (empty($result)) {
+            var_dump($result, curl_error($ch));
+            curl_close($ch);
+            return false;
+        }
+
+        curl_close($ch);
+        return $result;
+    }
+}
+
+?>
