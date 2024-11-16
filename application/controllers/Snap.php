bc3751bbf5e0bffc9c98289899a83a43f6f197fa
Fahmi <fahmi4331@gmail.com>
2024-11-16 14:07:44 +0800

    perubahan pubkey

--- application/controllers/Snap.php
+++ application/controllers/Snap.php
@@ -7,33 +7,12 @@ class Snap extends CI_Controller {
         $this->load->library('SnapOAuth');
     }
 
-    public function send_notification() {
-        // Data yang akan dikirim ke API
-        $data = [
-            'partnerServiceId' => '___77777',
-            'customerNo' => '08577508881',
-            'virtualAccountNo' => '___7777708577508881',
-            'trxDateTime' => '2023-12-04T08:34:00+07:00',
-            'paymentRequestId' => '2027912345671234567',
-            'additionalInfo' => [
-                'paymentAmount' => '650000',
-                'terminalId' => '1',
-                'bankId' => '002'
-            ]
-        ];
-
-        // Mengirim notifikasi menggunakan library SnapOAuth
-        $response = $this->snapoauth->notifyPaymentIntrabank($data);
-
-        // Menampilkan response
-        echo json_encode($response);
-    }
-
-    // Fungsi baru untuk validasi status transaksi
-    public function validate_transaction($transactionId) {
-        $response = $this->snapoauth->validateTransactionStatus($transactionId);
-
-        // Menampilkan hasil validasi
-        echo json_encode($response);
+    public function access_token_b2b()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            show_404();
+        }
+        $postData = $this->input->raw_input_stream;
+        echo json_encode(['status' => 'success', 'message' => 'Request berhasil diproses']);
     }
 }
