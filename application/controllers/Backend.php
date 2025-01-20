94457d28a1c7e39baa727ba7f2c8608a3c15a1a9
Fahmi <fahmi4331@gmail.com>
2025-01-20 10:41:40 +0800

    Typo

--- application/controllers/Backend.php
+++ application/controllers/Backend.php
@@ -105,7 +105,7 @@ public function notifikasi() {
         'X-PARTNER-ID' => $this->input->get_request_header('X-PARTNER-ID', TRUE),
         'CHANNEL-ID' => $this->input->get_request_header('CHANNEL-ID', TRUE),
         'X-EXTERNAL-ID' => $this->input->get_request_header('X-EXTERNAL-ID', TRUE),
-        'Content-type' => $this->input->get_request_header('Content-type', TRUE),
+        'Content-Type' => $this->input->get_request_header('Content-Type', TRUE),
     ];
 
     // Validasi keberadaan header
@@ -164,7 +164,7 @@ public function notifikasi() {
             $headers['X-PARTNER-ID'], 
             $headers['CHANNEL-ID'], 
             $headers['X-EXTERNAL-ID'],
-            $headers['Content-type']
+            $headers['Content-Type']
         );
         
         if ($validationResult['status'] === 'success') {
