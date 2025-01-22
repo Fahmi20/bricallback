299af35025b0470fe6b48e33c5b239b37ba7237b
Fahmi <fahmi4331@gmail.com>
2025-01-22 10:32:38 +0800

    sha256

--- application/libraries/Api.php
+++ application/libraries/Api.php
@@ -166,12 +166,8 @@ public function validateSignature($Authorization, $body, $timeStamp, $signature)
     $accessToken = $Authorization;  // Token akses yang diterima
     $clientSecret = $this->client_secret_push_notif_url;  // Secret key
 
-    // Mengubah body menjadi JSON dan minifikasi (hapus spasi)
-    $bodyJson = json_encode($body, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-    $bodyMinified = preg_replace('/\s+/', '', $bodyJson);
-
     // Menghitung hash SHA-256 dari body yang telah minified
-    $bodySHA256 = hash('sha256', $bodyMinified);
+    $bodySHA256 = hash('sha256');
 
     // Membentuk string yang akan digunakan untuk menghitung signature
     $stringToSign = $httpMethod . ":" . $path . ":" . $accessToken . ":" . $bodySHA256 . ":" . $timeStamp;
